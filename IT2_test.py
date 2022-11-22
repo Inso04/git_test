71416729a81075d0fed6aed7c04b1a1d5a703020
Inso04 <ingrid.grete04@gmail.com>
2022-11-22 13:04:12 +0100

    gjorde oppgave 2

--- IT2_test.py
+++ IT2_test.py
@@ -6,8 +6,23 @@ def pop(liste):
     #fjerner et element fra slutten av listen
     liste.remove(liste[len(liste)- 1])
 
+def rem(liste, antall):
+    #fjerner så mange elementer man vil fra listen
+    length = len(liste)
+    for i in range(antall):
+        liste.remove(liste[(length - antall)])
+
 handlekurv = ["ost", "paprika", "brød", "kaviar"]
 push(handlekurv, "ketchup")
 handlekurv[3] = "knekkebrød"
 pop(handlekurv)
 print(handlekurv)
+
+tretyper = ["lønn", "hassel", "bjørk", "eik", "blodlønn", "furu", "gran"]
+tretyper.remove("eik")
+tretyper[2] = "hengebjørk"
+rem(tretyper, 2)
+tretyper.append("fjellbjørk")
+tretyper.append("dvergbjørk")
+tretyper = sorted(tretyper)
+print(tretyper)
