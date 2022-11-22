5f396da07601ca28267488ae5aae9a91c7943220
Inso04 <ingrid.grete04@gmail.com>
2022-11-22 12:39:26 +0100

    gjorde oppgave 1

--- IT2_test.py
+++ IT2_test.py
@@ -1,3 +1,13 @@
+def push(liste, ting):
+    #legger til et element på starten av listen
+    liste.insert(0, ting)
 
-handleliste = ["ost", "paprika", "brød", "kaviar"]
-print(handleliste)
+def pop(liste):
+    #fjerner et element fra slutten av listen
+    liste.remove(liste[len(liste)- 1])
+
+handlekurv = ["ost", "paprika", "brød", "kaviar"]
+push(handlekurv, "ketchup")
+handlekurv[3] = "knekkebrød"
+pop(handlekurv)
+print(handlekurv)
