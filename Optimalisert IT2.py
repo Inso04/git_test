e046277135761b73e82802075509d468a45864a6
Inso04 <ingrid.grete04@gmail.com>
2022-11-28 08:54:00 +0100

    optimalisert oppg. 2

--- Optimalisert IT2.py
+++ Optimalisert IT2.py
@@ -8,3 +8,17 @@ def handleliste():
     print(handleliste)
 
 handleliste()
+
+#Oppgave 2
+def tretyper():
+    tre = ["lønn", "hassel", "bjørk", "eik", "blodlønn", "furu", "gran"]
+    tre.remove("eik")
+    tre[2] = "hengebjørk"
+    tre.remove("furu")
+    tre.remove("gran")
+    tre.append("fjellbjørk")
+    tre.append("dvergbjørk")
+    tre = sorted(tre)
+    print(tre)
+
+tretyper()
