21babdb1ba0d264de7f2133469e1dd44b508afea
Inso04 <ingrid.grete04@gmail.com>
2022-12-08 09:08:48 +0100

    testet noen små ting

--- barnehagen.py
+++ barnehagen.py
@@ -2,5 +2,7 @@ from storken import Person
 
 Suukhii = Person("Kvinne", 151, 18, "svart", "brun", "lilla")
 Ingrid = Person("Kvinne", 175, 18, "brun", "blå", "lilla")
+Mats = Person("Mann", 196, 18, "brun", "blå", "lyse lilla")
+Bjørn = Person("Mann", 190, 18, "brun", "blå", "grønn")
 
 Suukhii.Gå()
